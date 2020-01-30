20209194b6a3cc229a149de4ef8f937fe59eb922
Bruno <brunokoga187@gmail.com>
2020-01-29 23:43:35 -0300

    first commit teste okay

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Plugins, PluginListenerHandle, NetworkStatus } from '@capacitor/core'
+
+const { Network } = Plugins
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
+
+  networkStatus: NetworkStatus;
+  networkListener: PluginListenerHandle;
+
+  async ngOnInit() {
+    this.networkListener = Network.addListener('networkStatusChange', status => {
+      console.log('Network status changed', status);
+      this.networkStatus = status;
+    })
+
+    this.networkStatus = await Network.getStatus();
+  }
+
+  ngOnDestroy() {
+    this.networkListener.remove();
+  }
 
   constructor() {}
 
